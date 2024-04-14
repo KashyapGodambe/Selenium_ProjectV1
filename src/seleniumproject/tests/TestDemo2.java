48f335775a702bb37ec6539751fe205457cbc2c8
KashyapGodambe <kashgodambe@gmail.com>
2024-04-14 15:57:54 +0530

    Add the TestDemo2 class

---
+++ src/seleniumproject/tests/TestDemo2.java
@@ -0,0 +1,21 @@
+package seleniumproject.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+        
+        // Open a website
+        driver.get("https://app.vwo.com/#/login");
+        
+        driver.findElement(By.id("login-username")).sendKeys("kashgodambe@gmail.com");
+        driver.findElement(By.id("login-password")).sendKeys("Kashyap@123");
+
+	}
+
+}
